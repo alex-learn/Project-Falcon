b31e95abf5d85a60a422e98a2a7156d2a604d731
Sid Viswanathan <sid@cardmunch.com>
2013-01-05 14:59:25 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Falcon::Application.config.secret_token = 'f3e7b2891f1a1641465bf6bc8d1bc795400cab9d22683c5446c1d5d98c58faa5008554db7665be63c078abfa2ca19f5f357b3c58d4240ab6d56696756b1f6cad'
